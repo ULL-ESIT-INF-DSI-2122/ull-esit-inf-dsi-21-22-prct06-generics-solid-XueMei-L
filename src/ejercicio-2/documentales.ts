485b5045468086fb418febea6711e5d5a61b1735
XueMei Lin <alu0101225845@ull.edu.es>
2022-03-22 23:52:39 +0000

    tests para la subclase documentales

--- src/ejercicio-2/documentales.ts
+++
@@ -1,9 +0,0 @@
-import {BasicStreamableCollection} from '../../src/ejercicio-2/BasicStreamableCollection'
-
-export type documental = {
-
-}
-
-export class Documentales extends BasicStreamableCollection<documental> {
-    
-}
--- tests/ejercicio-2/ejericico-2.spec.ts
+++ tests/ejercicio-2/ejericico-2.spec.ts
@@ -3,17 +3,12 @@ import { expect } from 'chai';
 import { Streamable } from '../../src/ejercicio-2/Streamable';
 import { typeFilm, Films } from '../../src/ejercicio-2/peliculas';
 import { typeSerie, Series } from '../../src/ejercicio-2/series';
+import { typeDocumentary, Documentary } from '../../src/ejercicio-2/documentary';
 
 describe('Tests for ejercicio-2 Dsifix', () =>{
     
     describe('Tests for interface Streamble', () => {
-        // export type typeSerie = {
-        //     tilte:string;
-        //     year:number;
-        //     season:number;
-        //     star:number;
-        //     type:string;
-        // }
+
         let filmShrek:typeFilm = { title:'shrek2', year: 2004, type:'science fiction'};
         let filmTheMatrix:typeFilm = { title:'The Matrix', year:1999, type:'science fiction'};
         let filmFastAndFurious9:typeFilm = { title:'Fast And Furious', year:2021, type:'action'};
@@ -21,7 +16,9 @@ describe('Tests for ejercicio-2 Dsifix', () =>{
         let seriesPrisonBreak1:typeSerie = { tilte:'Prinson Break 01', year:2005, season:1, star:4.8, type:'criminal'};
         let seriesPrisonBreak2:typeSerie = { tilte:'Prinson Break 02', year:2006, season:2, star:4.7, type:'criminal'};
         let seriesPrisonBreak3:typeSerie = { tilte:'Prinson Break 03', year:2015, season:3, star:3.0, type:'criminal'};
-    
-        
+      
+        let documentaryStreetFood:typeDocumentary = { title:'Street Fodd', year:2019, type:'culture'};
+
+
     });
 });
