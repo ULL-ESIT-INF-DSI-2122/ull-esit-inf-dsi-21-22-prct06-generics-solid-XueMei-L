cc455f0a12c8d935ec2f24845ff1389bc7c18f13
XueMei Lin <alu0101225845@ull.edu.es>
2022-03-23 19:27:51 +0000

    la subclase Films finalizada, con typedoc

--- src/ejercicio-2/films.ts
+++ src/ejercicio-2/films.ts
@@ -1,6 +1,8 @@
 import {BasicStreamableCollection} from '../../src/ejercicio-2/BasicStreamableCollection'
-import { Series } from './series';
 
+/**
+ * TypeFilm to describe informations of Film
+ */
 export type typeFilm = {
     title:string;
     year:number;
@@ -8,14 +10,26 @@ export type typeFilm = {
     region:string;
 }
 
+/**
+ * Class Films to describe Films collection
+ */
 export class Films extends BasicStreamableCollection<typeFilm> {
     
     constructor(private collectionFilms:typeFilm[]) {
         super(collectionFilms);
     }
-
+    /**
+     * _Method that to get Films objects_
+     * @returns Films objects
+     */
     getElement() { return this.collectionFilms; }
 
+
+    /**
+     * _Method that to seach Films with corresponding information_
+     * @param data information that to seach
+     * @param value value to seach
+     */
     searchElement(data:string, value:string):typeFilm[]{
         let result:typeFilm[] = [];
         switch(data) {
