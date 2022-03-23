18ca9d43b6c67c1515d0318b71a74a33c4819a82
XueMei Lin <alu0101225845@ull.edu.es>
2022-03-23 19:41:38 +0000

    ejercicio2 terminado con la documentacion

--- src/ejercicio-2/BasicStreamableCollection.ts
+++ src/ejercicio-2/BasicStreamableCollection.ts
@@ -1,20 +1,37 @@
 import { StreamableSeach } from "./StreamableSeach";
 import { Streamable } from './Streamable'
 
-// class abstracta use implements
-// Definir constructor
-// Devolver la longitud de la colección
-// Definir el método de búsqueda general
-// Definir el método de imprimir resultado
+/**
+ * Abstract class BasicStreamableCollection to include all Streamables
+ */
 export abstract class BasicStreamableCollection<T> implements StreamableSeach<T>, Streamable<T> {
     
+    /**
+     * _Constructor of the abstract class_
+     * @param collection Array to save objects of type T
+     */
     constructor(protected collection:T[]) {}
-    
+
+    /**
+     * _Method that to add a object type T to the collection_
+     * @param element elemento type T to add to the collection
+     */
     addElement(element:T) { this.collection.push(element); }
     
-    // Obliga a subclases a implementar los metodos - No se implementa
+
+    // Methods that should be implement in subclases;
+
+    /**
+     * _Method that to get element of the colletion_
+     */
     abstract getElement():T[];
 
+
+    /**
+     * _Method that to seach type T with corresponding information_
+     * @param data information that to seach
+     * @param value value to seach
+     */
     abstract searchElement(name:string, type:string):T[];
 
 }
--- src/ejercicio-2/Streamable.ts
+++ src/ejercicio-2/Streamable.ts
@@ -1,3 +1,6 @@
+/**
+ * _Interface that has the methods addElements() and getElement() to implement_
+ */
 export interface Streamable<T> {
     addElement(elemento:T):void;
     getElement():T[];
