1dc8be825f9e1520a21ca825dfe4577e52c59cca
XueMei Lin <alu0101225845@ull.edu.es>
2022-03-23 00:16:15 +0000

    la subclase Documentary implementada, con metodos basicos implementados

--- src/ejercicio-2/documentary.ts
+++ src/ejercicio-2/documentary.ts
@@ -2,8 +2,23 @@ import {BasicStreamableCollection} from './BasicStreamableCollection'
 
 export type typeDocumentary = {
     
+    title:string;
+    year:number;
+    type:string;
 }
 
 export class Documentary extends BasicStreamableCollection<documentary> {
-    
+
+    constructor(elements:typeDocumentary[]) {
+        super(elements);
+    }
+    search(titleFilm:string, type:string):any {
+
+    }
+
+    print():void{
+        this.collection.forEach((tipo, value) =>{
+            
+        });
+    }
 }
