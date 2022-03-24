984b4a7a7cd91578adf87ca788efb454ef8b63a8
XueMei_L <alu0101225845@ull.edu.es>
2022-03-24 09:26:49 +0000

    modificacion de la PE (init)

---
+++ tests/modificacion/modification.spec.ts
@@ -0,0 +1,14 @@
+import 'mocha';
+import { expect } from 'chai';
+import { Pokedex } from "../../src/ejercicio-1/pokedex";
+
+
+describe('Tests for the class Pokedex', () => {
+
+    it('Existe una clase Pokedex', () => {
+        expect(Pokedex).not.to.be.null;
+    });
+
+    it('Existe objetos instanciados de la clase Pokedex', () => {
+    })
+});
